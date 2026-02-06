551c0de3417ade4110210c76406067e1899ddd36
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-02-06 22:50:55 +1100

    completed add book function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const library = [];
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = status;
+    this.read = false;
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages, status) {
-  const book = new Book(title, author, pages, status);
+function addBookToLibrary(title, author, pages) {
+  const book = new Book(title, author, pages);
   library.push(book);
 }
 
@@ -19,16 +19,47 @@ const createCard = (book) => {
     card.querySelector(".author").textContent = book.author;
     card.querySelector(".pages").textContent = book.pages;
     card.querySelector(".status").textContent = book.read;
+    card.dataset.bookId = book.id;
     return card;
 }
 
 const displayBooks = () => {
-    const page = document.querySelector(".reading");
+    const page = document.querySelector(".shelf");
+    page.innerHTML = '';
     for (const book of library) {
         page.appendChild(createCard(book));
     }
 }
 
-addBookToLibrary("Hello", "John", 123, false);
-addBookToLibrary("Goodbye", "Jane", 456, true);
+const dialog = document.querySelector("dialog");
+const addBtn = document.querySelector(".new");
+
+addBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+const submit = document.querySelector(".add");
+const cancel = document.querySelector(".cancel");
+const form = document.querySelector("form");
+
+submit.addEventListener("click", (e) => {
+    const title = document.querySelector(".book-title").value;
+    const author = document.querySelector(".book-author").value;
+    const pages = document.querySelector(".book-pages").value;
+    e.preventDefault();
+    if (form.checkValidity()) {
+        addBookToLibrary(title, author, pages);
+        displayBooks();
+        dialog.close();
+    } else {
+        form.reportValidity();
+    }
+});
+
+cancel.addEventListener("click", () => {
+    dialog.close();
+});
+
+addBookToLibrary("Hello", "John", 123);
+addBookToLibrary("Goodbye", "Jane", 456);
 displayBooks();
